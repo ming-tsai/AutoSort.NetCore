4393528eeddf253e30369c3975b78788ea7be968
Ming Tsai <ming.jia1213@gmail.com>
2020-06-21 13:12:35 -0400

    chore: add attribute and enums

---
+++ src/NetCore.AutoSort/Attributes/SortAttribute.cs
@@ -0,0 +1,24 @@
+﻿using NetCore.AutoSort.Enums;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace NetCore.AutoSort.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property|AttributeTargets.Class)]
+    public class SortAttribute : Attribute
+    {
+        public string PropertyName { get; private set; }
+        public SortDirection Direction { get; private set; }
+        public SortAttribute()
+        {
+        }
+
+        public SortAttribute(string propertyName, SortDirection direction = SortDirection.Ascending)
+        {
+            PropertyName = propertyName;
+            Direction = direction;
+        }
+    }
+}
