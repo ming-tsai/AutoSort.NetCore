99421c80ce653ab7aa0bb2832fcf3d9d5a871c87
Ming Tsai <ming.jia1213@gmail.com>
2020-06-21 13:13:03 -0400

    chore: add general test for sort attribute

---
+++ test/Attributes/Sort/General.cs
@@ -0,0 +1,28 @@
+﻿using NetCore.AutoSort.Attributes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore.AutoSort.Test.Attributes.Sort
+{
+    public class General
+    {
+        [Test]
+        public void InstanceTheAttribute_ShouldNotBeMultiple()
+        {
+            var attributes = (IList<AttributeUsageAttribute>)typeof(SortAttribute)
+                        .GetCustomAttributes(typeof(AttributeUsageAttribute), false);
+            Assert.AreEqual(1, attributes.Count);
+        }
+
+        [TestCase("PropertyNameA")]
+        [TestCase("PropertyNameB")]
+        [TestCase("PropertyNameC")]
+        public void PassPropertyName_ShouldExpectedPropertyName(string name)
+        {
+            var attribute = new SortAttribute(name);
+            Assert.AreEqual(name, attribute.PropertyName);
+        }
+    }
+}
