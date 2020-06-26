26f26d11857f620aee10af1dce2d45e980ec851d
Ming Tsai <ming.jia1213@gmail.com>
2020-06-26 16:52:53 -0400

    chore(sortable): change sortable property constructor

--- src/NetCore.AutoSort/SortableProperty.cs
+++ src/NetCore.AutoSort/SortableProperty.cs
@@ -1,4 +1,7 @@
 ﻿using NetCore.AutoSort.Attributes;
+using NetCore.AutoSort.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
 namespace NetCore.AutoSort
@@ -6,11 +9,32 @@ namespace NetCore.AutoSort
     public sealed class SortableProperty
     {
         public PropertyInfo Property { get; private set; }
-        public SortAttribute Sort { get; private set; }
+        public int Order { get; set; }
+        public SortDirection Direction { get; private set; }
+
+        public SortableProperty(
+            [Range(0, int.MaxValue)] int order, PropertyInfo property, SortDirection direction)
+        {
+            if (order < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(order));
+            }
+
+            Order = order;
+            Property = property;
+            Direction = direction;
+        }
+
         public SortableProperty(PropertyInfo property, SortAttribute sort)
         {
+            if (sort == null)
+            {
+                throw new ArgumentNullException(nameof(sort));
+            }
+
             Property = property;
-            Sort = sort;
+            Order = sort.Order;
+            Direction = sort.Direction;
         }
     }
 }
