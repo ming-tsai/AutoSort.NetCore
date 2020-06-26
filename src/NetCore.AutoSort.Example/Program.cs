a2597be9e219ec4ccb76fb57f5c99fd8d4e37e3c
Ming Tsai <ming.jia1213@gmail.com>
2020-06-26 17:05:49 -0400

    chore(example): add example on project

--- src/NetCore.AutoSort.Example/Program.cs
+++ src/NetCore.AutoSort.Example/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using NetCore.AutoSort.Example.InMemoryData;
+using NetCore.AutoSort.Example.Model;
+using System;
+using System.Linq;
+using System.Text.Json;
 
 namespace NetCore.AutoSort.Example
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // Applying using sort property.
+            // If not passing string sort it will take out data that is on sort property
+            var people = new PersonCollection();
+
+            // Apply sort
+            var sorted = people.AsQueryable().ApplySort();
+            WriteCollection(sorted);
+
+            // Using implicit
+            sorted = new PersonCollection().AsQueryable().ApplySort();
+            WriteCollection(sorted);
+
+            // Using string sort
+            sorted = new PersonCollection().AsQueryable().ApplySort("FirstName, LastName");
+            WriteCollection(sorted);
+
+
+            // TODO: Pass unexists property name it won't launch exception
+            sorted = new PersonCollection().AsQueryable().ApplySort("FirstName, LastName desc, SomeProperty");
+            WriteCollection(sorted);
+        }
+
+        private static void WriteCollection(IQueryable<Person> sorted)
+        {
+            Console.WriteLine(JsonSerializer.Serialize(sorted));
         }
     }
 }
